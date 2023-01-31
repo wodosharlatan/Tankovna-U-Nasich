7c4b3781062330dec2bb6e40aed0bd4d757179e0
Tomáš Bosák <Tom.bos@email.cz>
2023-02-01 00:04:01 +0100

    Alpha 1.02
    Content Update + CSS Styling changes, Comments for better orientation in project

--- js/custom.js
+++ js/custom.js
@@ -34,14 +34,6 @@ $(document).ready(function() {
     $('select').niceSelect();
   });
 
-/** google_map js **/
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
 
 // client section owl carousel
 $(".client_owl-carousel").owlCarousel({
@@ -68,3 +60,6 @@ $(".client_owl-carousel").owlCarousel({
         }
     }
 });
+
+
+
