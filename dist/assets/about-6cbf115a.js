29fee9af3b61d0f58febce71650c425cabd62d15
Tomáš Bosák <tombos255@gmail.com>
2023-03-14 23:52:21 +0100

    Alpha 1.08
    Routing Build On Netlify

---
+++ dist/assets/about-6cbf115a.js
@@ -0,0 +1,1 @@
+import{j as o,c as t}from"./client-8ab449ee.js";function e(){return o("h1",{children:"Hello From About"})}function n(){return o(e,{})}t.createRoot(document.getElementById("root")).render(o(n,{}));
---
+++ dist/assets/contact-58bdd786.js
@@ -0,0 +1,1 @@
+import{j as t,c as n}from"./client-8ab449ee.js";function o(){return t("h1",{children:"Hello From Contact"})}function e(){return t(o,{})}n.createRoot(document.getElementById("root")).render(t(e,{}));
