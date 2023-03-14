a3694c2d42a98f4b213db11912bb2aca02351883
Tomáš Bosák <tombos255@gmail.com>
2023-03-14 23:16:48 +0100

    Alpha 1.06
    Simple Vite Routing / Multi Page app

---
+++ bar-u-nasich/src/about/about-main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AboutComponent from "./about";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<AboutComponent />);
---
+++ bar-u-nasich/src/about/about.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function AboutComponent() {
+	return <h1>Hello From About</h1>;
+}
+
+export default AboutComponent;
+ 
