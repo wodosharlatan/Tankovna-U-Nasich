4fdb9568072b5940c9ae0c930ef580466e4c81c5
Tomáš Bosák <85733005+wodosharlatan@users.noreply.github.com>
2023-03-21 08:58:42 +0000

    Path Changes

--- src/components/gallery/ImprovedGallery.jsx
+++ src/components/gallery/ImprovedGallery.jsx
@@ -1,66 +1,66 @@
 import React from "react";
-import {Gallery} from "./Gallery.jsx";
+import { Gallery } from "./GalleryFunction";
 
 function ImprovedGallery() {
 
 
-  const galleryImages = [
+	const galleryImages = [
 		{
-			img: "../public/Images/0.png",
-    },
-		{
-			img: "../public/Images/1.png",
+			img: "../Images/5.png",
 		},
 		{
-			img: "../public/Images/2.png",
+			img: "../Images/1.png",
 		},
 		{
-			img: "../public/Images/3.png",
+			img: "../Images/2.png",
 		},
 		{
-			img: "../public/Images/4.png",
+			img: "../Images/4.png",
 		},
 		{
-			img: "../public/Images/5.png",
+			img: "../Images/11.png",
 		},
 		{
-			img: "../public/Images/6.png",
+			img: "../Images/0.png",
 		},
 		{
-			img: "../public/Images/7.png",
+			img: "../Images/6.png",
 		},
 		{
-			img: "../public/Images/8.png",
+			img: "../Images/7.png",
 		},
 		{
-			img: "../public/Images/9.png",
+			img: "../Images/8.png",
 		},
 		{
-			img: "../public/Images/10.png",
+			img: "../Images/9.png",
 		},
 		{
-			img: "../public/Images/11.png",
+			img: "../Images/10.png",
 		},
 		{
-			img: "../public/Images/12.png",
+			img: "../Images/3.png",
 		},
 		{
-			img: "../public/Images/13.png",
+			img: "../Images/12.png",
 		},
 		{
-			img: "../public/Images/14.png",
+			img: "../Images/13.png",
 		},
 		{
-			img: "../public/Images/15.png",
+			img: "../Images/14.png",
 		},
 		{
-			img: "../public/Images/16.png",
+			img: "../Images/15.png",
 		},
 		{
-			img: "../public/Images/17.png",
+			img: "../Images/16.png",
 		},
 		{
-			img: "../public/Images/18.png",
+			img: "../Images/17.png",
+		},
+		{
+			img: "../Images/18.png",
 		},
 	];
 	return (
