2e84f9c3cfdf590fe50449db49a03d8e8d02fc6d
matejmichalek2004 <mata.michalek@gmail.com>
2023-03-23 20:24:40 +0100

    gallery all images same size

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -5,13 +5,11 @@ function About() {
 
 
 	return (
-		<>
-			<div className="container">
+		<div className="about-page container">
 
-			<ImprovedGallery/>
-				
-			</div>
-		</>
+			<ImprovedGallery />
+
+		</div>
 	);
 }
 
