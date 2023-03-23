e618d918b5655baf2101ea606bb29ea34c049a96
matejmichalek2004 <mata.michalek@gmail.com>
2023-03-23 20:55:07 +0100

    welcome component added

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,4 +1,5 @@
 import ImprovedGallery from "../../components/gallery/ImprovedGallery";
+import Welcome from "../../components/welcome/welcome";
 import "./about.scss";
 
 function About() {
@@ -6,11 +7,47 @@ function About() {
 
 	return (
 		<div className="about-page container">
-
+			<Welcome />
 			<ImprovedGallery />
-
 		</div>
 	);
 }
-
 export default About;
+
+
+// const l = [
+// 	{
+// 		name: "Nealko",
+// 		products: [
+// 			{
+// 				name: "Coca Cola",
+// 				price: 20
+// 			},
+// 			{
+// 				name: "Fanta",
+// 				price: 20
+// 			},
+// 			{
+// 				name: "Sprite",
+// 				price: 20
+// 			}
+// 		]
+// 	},
+// 	{
+// 		name: "alko",
+// 		products: [
+// 			{
+// 				name: "Coca Cola",
+// 				price: 20
+// 			},
+// 			{
+// 				name: "Fanta",
+// 				price: 20
+// 			},
+// 			{
+// 				name: "Sprite",
+// 				price: 20
+// 			}
+// 		]
+// 	}
+// ]
