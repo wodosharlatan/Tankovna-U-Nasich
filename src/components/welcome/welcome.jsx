7fb9ab3d2491e17373b2498ab3757e0fee481b12
Tomáš Bosák <tombos255@gmail.com>
2023-03-25 14:22:52 +0100

    Small Menu And Landing Page Changes

--- src/components/welcome/welcome.jsx
+++ src/components/welcome/welcome.jsx
@@ -1,27 +1,28 @@
-import './welcome.scss';
-
+import "./welcome.scss";
 
 const Welcome = () => {
-    return (
-        <div className="welcome">
-            <div className="left">
-                <img src='https://i.seadn.io/gae/2hDpuTi-0AMKvoZJGd-yKWvK4tKdQr_kLIpB_qSeMau2TNGCNidAosMEvrEXFO9G6tmlFlPQplpwiqirgrIPWnCKMvElaYgI-HiVvXc?auto=format&w=1000' alt='random' />
-            </div>
+	return (
+		<div className="welcome">
+			<div className="left">
+				<img src="https://i.seadn.io/gae/2hDpuTi-0AMKvoZJGd-yKWvK4tKdQr_kLIpB_qSeMau2TNGCNidAosMEvrEXFO9G6tmlFlPQplpwiqirgrIPWnCKMvElaYgI-HiVvXc?auto=format&w=1000" alt="random"/>
+			</div>
 
-            <div className="right">
-                <h1 className="welcome__title">Pijte a Ochutnejte!</h1>
-                <div className="about__text">
-                    <p>
-                        Vítejte v našem Baru, kde nabízíme široký výběr piv a tvrdých
-                        alkoholů. Doplníme váš zážitek s našimi malými pochutinami. U nás
-                        si můžete vychutnat dobré pivo s přáteli nebo si dát silný drink
-                        na zahřátí. Nenechte si ujít atmosféru našeho baru a přijďte si
-                        užít chvíle plné dobrého pití a jídla. Těšíme se na vaši návštěvu!
-                    </p>
-                </div>
-            </div>
-        </div>
-    );
-}
+			<div className="right">
+				<h1 className="section__title">
+					Pijte a Ochutnejte!
+				</h1>
+				<div className="about__text">
+					<p>
+						Vítejte v našem Baru, kde nabízíme široký výběr piv a tvrdých
+						alkoholů. Doplníme váš zážitek s našimi malými pochutinami. U nás si
+						můžete vychutnat dobré pivo s přáteli nebo si dát silný drink na
+						zahřátí. Nenechte si ujít atmosféru našeho baru a přijďte si užít
+						chvíle plné dobrého pití a jídla. Těšíme se na vaši návštěvu!
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Welcome;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,17 @@
 import './home.scss';
+import Welcome from '../../components/welcome/welcome.jsx'
+import Menu from '../menu/menu';
+import Reservation from '../reservation/reservation';
 
 const Home = () => {
     return (
         <div className="container">
             <h1>Home</h1>
+            <Welcome />
+            <hr />
+            <Menu />
+            <hr />
+            <Reservation />
         </div>
     );
 }
