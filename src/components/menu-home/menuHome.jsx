e00b4f94a69ae6cf6629d1523887b7f8f490cc76
Tomáš Bosák <85733005+wodosharlatan@users.noreply.github.com>
2023-03-31 08:46:36 +0000

    Home Menu Content Change

--- src/components/menu-home/menuHome.jsx
+++ src/components/menu-home/menuHome.jsx
@@ -5,26 +5,29 @@ import { IoArrowForwardOutline } from "react-icons/io5";
 function HomeMenu() {
 	return (
 		<>
-			<div className="welcome">
+			<div className="homeMenu">
 				<div className="left">
-					<img src="https://media.tenor.com/Dc5SGhuskTUAAAAC/old-guy-falling-down-stairs.gif" alt="random" />
-				</div>
-
-				<div className="right">
-					<h1 className="section__title">Bruh Jsou 2 ráno!</h1>
-					<div className="about__text">
+					<h1 className="section__title"> Menu MENU (so true(real))!</h1>
+					<div className="homeMenu__text">
 						<p>
 							Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut consequatur dolores consequuntur alias illum labore, corrupti tempora nostrum quisquam omnis ex, quibusdam sed porro reiciendis, nam magni saepe cupiditate at!
 						</p>
 
 						<div className="button_div">
-							<button className="welcome_button">
-								<a href="/about/"> Zobrazit Nápojový Lístek </a>
+							<button className="homeMenu_button">
+								<a href="/menu/"> Zobrazit Nápojový Lístek </a>
 								<IoArrowForwardOutline className="icon" />
 							</button>
 						</div>
 					</div>
 				</div>
+
+
+				<div className="right">
+					<img src="https://media.tenor.com/Dc5SGhuskTUAAAAC/old-guy-falling-down-stairs.gif" alt="random" />
+				</div>
+
+
 			</div>
 		</>
 	);
